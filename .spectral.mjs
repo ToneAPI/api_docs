1827797bcaadf96c60e1a746d61b7217198002c7
nathan.tienyou@viacesi.fr <legonzaur@live.fr>
2023-09-07 18:01:12 +0200

    Update v3 server routes
    Committed by Legonzaur on behalf of nathan.tienyou@viacesi.fr

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNzE2MA/spectral.js?branch_slug=main&token=a515322d-f84f-4214-baa1-fe7ddaaa64dd";
+export default { extends: ruleset };
